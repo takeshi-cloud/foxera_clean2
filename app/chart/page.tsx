5e6925ee6d7d75ce50f57d3b848345d202dedbdf
takeshi <takeshi@foxera.jp>
2026-04-19 22:02:34 +0900

    fix: client order

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,6 +1,6 @@
-
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
+
 
 import JournalChartSection from "./components/JournalChartSection";
 import { TradeResultPanel } from "./components/TradeResultPanel";
